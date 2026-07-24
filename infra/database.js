95de58be903570242d09779e446acd8e5ad74eb8
Eduardo Marinho <eduardo099m@gmail.com>
2026-07-24 18:12:23 -0300

    Adds the Github Action `Linting` with `eslint`.

--- infra/database.js
+++ infra/database.js
@@ -2,33 +2,32 @@ import { Client } from "pg";
 
 async function query(queryObject) {
   let client;
-  try{
-    client = await getNewClient()
-    const result = await client.query(queryObject)
+  try {
+    client = await getNewClient();
+    const result = await client.query(queryObject);
     return result;
-  }catch (error) {
-    console.error(error)
+  } catch (error) {
+    console.error(error);
     throw error;
-  }finally{
-    await client.end()
+  } finally {
+    await client.end();
   }
 }
-
-async function getNewClient(params) {
+async function getNewClient() {
   const client = new Client({
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-    ssl: process.env.NODE_ENV === "production" ? true : false
-  })
-  await client.connect()
+    ssl: process.env.NODE_ENV === "production" ? true : false,
+  });
+  await client.connect();
   return client;
 }
 
 const database = {
   query,
-  getNewClient
-}
+  getNewClient,
+};
 export default database;
