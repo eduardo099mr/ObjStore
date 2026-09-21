f91852e8058295ba22b17e104135852525bcc4da
Eduardo Marinho <eduardo099m@gmail.com>
2026-09-20 21:30:03 -0300

    feat: add a connection between endpoint `/status` and database
    add the file `jsoncfig.json`, the dependencie `pg` and a db connection to the project

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,3 +1,6 @@
+import database from "/infra/database";
+
 export default async function status(request, response){
-  response.status(200).json({ status: "OK" });
+  const num = await database.query("SELECT 1 + 1 as sum;");
+  response.status(200).json({ status: "OK", result: num.rows[0].sum });
 }
